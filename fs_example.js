59f75f6420c6bcf932a9c9b0a1db735efe7eba65
Haris Habib <hhsiddiqui@hotmail.com>
2023-08-14 20:48:10 +1000

    Committing changes.

--- fs_example.js
+++ fs_example.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 var files = fs.readdirSync('./');
+//var fileCreate = fs.createWriteStream('./', fs.appendFile,
+//var logFile = fs.writeFileSync('./test.log', files, );
+
 console.log(files);
